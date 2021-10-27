7469b321e112791b5f8240f8adf8e103fc3ad7f6
FesalBadday <fesal.n04@hotmail.com>
2021-10-26 20:37:54 -0600

    added font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '5.5xl': ['3.5rem', { lineHeight: '1' }],
+      }
+    },
   },
   variants: {
     extend: {},
